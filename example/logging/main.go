3ee3e31a91569c9c60388256395da511a5da1640
tylerweitzman <tylerweitzman@gmail.com>
2019-12-16 19:51:03 -0800

    rename imports

--- example/logging/main.go
+++ example/logging/main.go
@@ -10,7 +10,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/tylerweitzman/service"
 )
 
 var logger service.Logger
--- service_su_test.go
+++ service_su_test.go
@@ -18,7 +18,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/tylerweitzman/service"
 )
 
 const runAsServiceArg = "RunThisAsService"
